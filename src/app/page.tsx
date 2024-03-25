d91277f066e9411aaa98988ce0f95d1fdd0c6ef4
Diego Graffigna <diegograffigna1@gmail.com>
2024-03-25 16:33:09 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import AgeConfirmationModal from "@/components/AgeConfirmationModal";
-
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <AgeConfirmationModal />
       <h1 className="text-5xl">pagina principal</h1>
     </div>
   );
