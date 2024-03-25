ae8a5c944366f6d5c2794e1124d6fd5fa81bd082
Diego Graffigna <diegograffigna1@gmail.com>
2024-03-25 16:34:38 -0300

    wip

--- src/app/producto/[handle]/page.tsx
+++ src/app/producto/[handle]/page.tsx
@@ -1,5 +1,4 @@
 export default function Producto({ params }: { params: { handle: string } }) {
-export default function Producto({params}:{params: {handle: string}}) {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <h1 className="text-5xl">pagina Producto {params.handle}</h1>
