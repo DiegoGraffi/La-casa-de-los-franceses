766e8bb09ba7273f17b45549e1aa164a27e2cf5d
Diego Graffigna <diegograffigna1@gmail.com>
2024-04-15 19:27:18 -0300

    wip

--- src/app/homeSections/Productos.tsx
+++ src/app/homeSections/Productos.tsx
@@ -1,15 +1,18 @@
-import { fetchGraphql, graphql } from "../../../lib/graphql";
+import { fetchGraphql, graphql } from "@/lib/graphql";
 
 export default async function Productos() {
   const query = graphql(`
-	query products(first: 100) {
-		nodes {
-			title
-		}
-	}
-`);
+    query Productos {
+      products(first: 20) {
+        nodes {
+          title
+        }
+      }
+    }
+  `);
 
   const data = await fetchGraphql(query, {});
+  console.log(data.products);
 
   return (
     <div className="container mx-auto p-5 grid grid-cols-4">
