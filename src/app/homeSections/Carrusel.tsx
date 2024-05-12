ee56812519764e6294e6e91e1eca5171b22f27b5
jjuansimon <merlojuansimon@gmail.com>
2024-05-12 20:38:33 -0300

    Fix carousel

--- src/app/homeSections/Carrusel.tsx
+++ src/app/homeSections/Carrusel.tsx
@@ -15,46 +15,46 @@ export default function Carrusel() {
   return (
     <div className="max-h-[2000px] max-w-full py-[90px] mx-auto flex flex-col gap-[40px] justify-center items-center overflow-hidden">
       <SectionTitle title="Selección del mes" />
-      <div className="relative w-full px-8 mx-50">
+      <div className="relative w-full px-[80px]">
         <button
           onClick={() => slider.current && slider.current.prev()}
-          className="absolute left-0 z-20 bg-white p-2 rounded-full shadow top-1/2 transform -translate-y-1/2 -translate-x-1/2"
+          className="absolute left-0 z-20 rounded-full top-1/2 transform -translate-y-1/2 hover:bg-transparent hover:bg-primarioMuyClaro"
         >
-          <Image src={prevArrow} alt="Flecha Anterior" />
+          <Image src={prevArrow} alt="Flecha Anterior"/>
         </button>
-        <div ref={sliderRef} className="keen-slider mx-1">
+        <div ref={sliderRef} className="keen-slider mr-4">
           <div className="keen-slider__slide">
             <ProductCard
               price="300"
-              title="Un vinito"
+              title="Vinito 1"
               image={require("../../../public/images/vinoImagenProducto.png")}
             />
           </div>
           <div className="keen-slider__slide">
             <ProductCard
               price="300"
-              title="Un vinito"
+              title="Vinito 2"
               image={require("../../../public/images/vinoImagenProducto.png")}
             />
           </div>
           <div className="keen-slider__slide">
             <ProductCard
               price="300"
-              title="Un vinito"
+              title="Vinito 3"
               image={require("../../../public/images/vinoImagenProducto.png")}
             />
           </div>
           <div className="keen-slider__slide">
             <ProductCard
               price="300"
-              title="Un vinito"
+              title="Vinito 4"
               image={require("../../../public/images/vinoImagenProducto.png")}
             />
           </div>
         </div>
         <button
           onClick={() => slider.current && slider.current.next()}
-          className="absolute right-0 z-20 bg-white p-2 rounded-full shadow top-1/2 transform -translate-y-1/2 translate-x-1/2"
+          className="absolute right-0 z-20 rounded-full top-1/2 transform -translate-y-1/2 hover:bg-transparent hover:bg-primarioMuyClaro"
         >
           <Image src={nextArrow} alt="Flecha Siguiente" />
         </button>
